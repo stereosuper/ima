76567b2f9a762471497810e66fd9923368f428ca
ArnaudBan <abanvillet@studio-goliath.com>
2018-04-19 16:50:01 +0200

    Détail d’offre va chercher le bon interlocuteur

--- web/wp-content/themes/IMA/single-offres.php
+++ web/wp-content/themes/IMA/single-offres.php
@@ -117,39 +117,56 @@ get_header('rh'); ?>
 
 <section id="content-right">
 	<?php $pages = get_posts(array( 'post_type' => 'page', 'meta_key' => '_wp_page_template', 'meta_value' => 'postuler.php' ));
-		foreach($pages as $page){
-	?>
+	foreach($pages as $page){
+		$interlocuteurs = get_field( 'interlocuteur', $page->ID );
 
-		<?php if( get_field('interlocuteur_nom', $page->ID) ){ ?>
-			<div id="bloc-interlocuteur">
-				<span class="categ-bloc-interlocuteur">
-					<span class="container-fond-categ-bloc-interlocuteur">
-						<span class="fond-categ-bloc-interlocuteur"></span>
+		if ( $interlocuteurs ) {
+			foreach ( $interlocuteurs as $i ) {
+				?>
+				<div id="bloc-interlocuteur">
+					<span class="categ-bloc-interlocuteur">
+						<span class="container-fond-categ-bloc-interlocuteur">
+							<span class="fond-categ-bloc-interlocuteur"></span>
+						</span>
+						<span class="container-photo-bloc-interlocuteur">
+							<?php if ( get_field( 'interlocuteur_img', $i->ID ) ) {
+								?>
+								<span class="fond-photo-bloc-interlocuteur">
+									<img src="<?php echo get_field( 'interlocuteur_img', $i->ID )['sizes']['thumbnail']; ?>" alt="<?php echo get_field( 'interlocuteur_img', $i->ID )['alt']; ?>"/>
+								</span>
+							<?php } ?>
+						</span>
+						<span class="txt-categ-bloc-interlocuteur">Votre&nbsp;interlocuteur</span>
 					</span>
-					<span class="container-photo-bloc-interlocuteur">
-						<?php if( get_field('interlocuteur_img', $page->ID) ) { ?><span class="fond-photo-bloc-interlocuteur"><img src="<?php echo get_field('interlocuteur_img', $page->ID)['sizes']['thumbnail']; ?>" alt="<?php echo get_field('interlocuteur_img', $page->ID)['alt']; ?>"/></span><?php } ?>
+					<span class="container-fond-bloc-deform-interlocuteur">
+						<span class="fond-bloc-deform-interlocuteur"></span>
 					</span>
-					<span class="txt-categ-bloc-interlocuteur">Votre&nbsp;interlocuteur</span>
-				</span>
-				<span class="container-fond-bloc-deform-interlocuteur">
-					<span class="fond-bloc-deform-interlocuteur"></span>
-				</span>
-				<span class="txt-bloc-deform-interlocuteur">
-					<h3><?php the_field('interlocuteur_nom', $page->ID); ?></h3>
-					<ul class="contact-bloc-interlocuteur">
-						<?php if( get_field('interlocuteur_tel', $page->ID) ) { ?><li class="has-icon"><div class="icon-telephone"></div><?php the_field('interlocuteur_tel', $page->ID); ?></li><?php } ?>
-						<?php if( get_field('interlocuteur_mail', $page->ID) ) { ?><li class="has-icon"><div class="icon-mail"></div><a href="mailto:<?php the_field('interlocuteur_mail'); ?>"><?php the_field('interlocuteur_mail', $page->ID); ?></a></li><?php } ?>
-					</ul>
-				</span>
-				<a href="#" class="detail-bloc-interlocuteur btn-interlocuteur">
-					<span class="container-fond-detail-bloc-interlocuteur">
-						<span class="fond-detail-bloc-interlocuteur"></span>
+					<span class="txt-bloc-deform-interlocuteur">
+						<h3><?php the_field( 'interlocuteur_nom', $i->ID ); ?></h3>
+						<ul class="contact-bloc-interlocuteur">
+						<?php if ( get_field( 'interlocuteur_tel', $i->ID ) ) { ?>
+							<li class="has-icon">
+							<div class="icon-telephone"></div><?php the_field( 'interlocuteur_tel', $i->ID ); ?>
+							</li><?php } ?>
+							<?php if ( get_field( 'interlocuteur_mail', $i->ID ) ) { ?>
+								<li class="has-icon">
+								<div class="icon-mail"></div><a
+									href="mailto:<?php the_field( 'interlocuteur_mail' ); ?>"><?php the_field( 'interlocuteur_mail',
+										$i->ID ); ?></a></li><?php } ?>
+						</ul>
 					</span>
-					<span class="txt-detail-bloc-interlocuteur">En savoir +</span>
-				</a>
-			</div>
-		<?php } ?>
-	<?php } ?>
+					<a href="#" class="detail-bloc-interlocuteur btn-interlocuteur">
+						<span class="container-fond-detail-bloc-interlocuteur">
+							<span class="fond-detail-bloc-interlocuteur"></span>
+						</span>
+						<span class="txt-detail-bloc-interlocuteur">En savoir +</span>
+					</a>
+				</div>
+				<?php
+			}
+		}
+
+	} ?>
 
 </section>
 
@@ -163,55 +180,93 @@ get_header('rh'); ?>
 
 				<?php include('includes/sitemap.php'); ?>
 
-				<?php $pages = get_posts(array( 'post_type' => 'page', 'meta_key' => '_wp_page_template', 'meta_value' => 'postuler.php' ));
-					foreach($pages as $page){
-				?>
+				<?php
+				foreach($pages as $page){
+					$interlocuteurs = get_field( 'interlocuteur', $page->ID );
+					if ( $interlocuteurs ) {
+						foreach ( $interlocuteurs as $i ) {
 
-					<?php if( get_field('interlocuteur_nom', $page->ID) ){ ?>
-						<div id="wrapper-interlocuteur-modal">
-							<div id="wrapper-relative-interlocuteur-modal">
-								<div id="overlay-interlocuteur"></div>
-								<nav id="interlocuteur-modal">
-									<div class="container-fond-modal">
-										<div class="fond-modal"></div>
-									</div>
-									<div id="content-interlocuteur">
-										<div class="bloc-gauche-content-interlocuteur">
-											<div class="container-photo-interlocuteur"><div class="fond-photo-interlocuteur"><?php if( get_field('interlocuteur_img', $page->ID) ) { ?><img src="<?php echo get_field('interlocuteur_img', $page->ID)['sizes']['thumbnail']; ?>" alt="<?php echo get_field('interlocuteur_img', $page->ID)['alt']; ?>"/><?php } ?></div></div>
-										</div><div class="bloc-droite-content-interlocuteur">
-											<h3><?php the_field('interlocuteur_nom', $page->ID); ?></h3>
-											<?php if(get_field('interlocuteur_devise', $page->ID)) echo "<blockquote>". get_field('interlocuteur_devise', $page->ID) ."</blockquote>"; ?>
-											<ul class="contact-interlocuteur">
-												<?php if( get_field('interlocuteur_tel', $page->ID) ) { ?><li class="has-icon"><div class="icon-telephone"></div><?php the_field('interlocuteur_tel', $page->ID); ?></li><?php } ?>
-												<?php if( get_field('interlocuteur_mail', $page->ID) ) { ?><li class="has-icon"><div class="icon-mail"></div><a href="mailto:<?php the_field('interlocuteur_mail', $page->ID); ?>"><?php the_field('interlocuteur_mail', $page->ID); ?></a></li><?php } ?>
-												<li>
-													<ul class="social-interlocuteur">
-														<?php if( get_field('interlocuteur_twitter', $page->ID) ){ ?><li class="twitter"><a href="<?php the_field('interlocuteur_twitter', $page->ID); ?>" target="_blank"><span class="icon-twitter"></span></a></li><?php } ?>
-														<?php if( get_field('interlocuteur_linkedin', $page->ID) ){ ?><li class="linkedin"><a href="<?php the_field('interlocuteur_linkedin', $page->ID); ?>" target="_blank"><span class="icon-linkedin"></span></a></li><?php } ?>
-														<?php if( get_field('interlocuteur_fb', $page->ID) ){ ?><li class="facebook"><a href="<?php the_field('interlocuteur_fb', $page->ID); ?>" target="_blank"><span class="icon-facebook"></span></a></li><?php } ?>
-														<?php if( get_field('interlocuteur_viadeo', $page->ID) ){ ?><li class="viadeo"><a href="<?php the_field('interlocuteur_viadeo', $page->ID); ?>" target="_blank"><span class="icon-viadeo"></span></a></li><?php } ?>
-													</ul>
-												</li>
-											</ul>
+							?>
+							<div id="wrapper-interlocuteur-modal">
+								<div id="wrapper-relative-interlocuteur-modal">
+									<div id="overlay-interlocuteur"></div>
+									<nav id="interlocuteur-modal">
+										<div class="container-fond-modal">
+											<div class="fond-modal"></div>
 										</div>
-									</div>
-									<div class="titre-modal">
-										<div class="container-fond-titre-modal">
-											<div class="fond-titre-modal"></div>
+										<div id="content-interlocuteur">
+											<div class="bloc-gauche-content-interlocuteur">
+												<div class="container-photo-interlocuteur">
+													<div class="fond-photo-interlocuteur"><?php if ( get_field( 'interlocuteur_img',
+															$i->ID ) ) { ?><img src="<?php echo get_field( 'interlocuteur_img',
+															$i->ID )['sizes']['thumbnail']; ?>"
+													                            alt="<?php echo get_field( 'interlocuteur_img',
+														                            $i->ID )['alt']; ?>"/><?php } ?></div>
+												</div>
+											</div>
+											<div class="bloc-droite-content-interlocuteur">
+												<h3><?php the_field( 'interlocuteur_nom', $i->ID ); ?></h3>
+												<?php if ( get_field( 'interlocuteur_devise', $i->ID ) ) {
+													echo "<blockquote>" . get_field( 'interlocuteur_devise',
+															$i->ID ) . "</blockquote>";
+												} ?>
+												<ul class="contact-interlocuteur">
+													<?php if ( get_field( 'interlocuteur_tel', $i->ID ) ) { ?>
+														<li class="has-icon">
+														<div class="icon-telephone"></div><?php the_field( 'interlocuteur_tel',
+															$i->ID ); ?></li><?php } ?>
+													<?php if ( get_field( 'interlocuteur_mail', $i->ID ) ) { ?>
+														<li class="has-icon">
+														<div class="icon-mail"></div>
+														<a href="mailto:<?php the_field( 'interlocuteur_mail',
+															$i->ID ); ?>"><?php the_field( 'interlocuteur_mail', $i->ID ); ?></a>
+														</li><?php } ?>
+													<li>
+														<ul class="social-interlocuteur">
+															<?php if ( get_field( 'interlocuteur_twitter', $i->ID ) ) { ?>
+																<li class="twitter"><a
+																	href="<?php the_field( 'interlocuteur_twitter', $i->ID ); ?>"
+																	target="_blank"><span class="icon-twitter"></span></a>
+																</li><?php } ?>
+															<?php if ( get_field( 'interlocuteur_linkedin', $i->ID ) ) { ?>
+																<li class="linkedin"><a
+																	href="<?php the_field( 'interlocuteur_linkedin', $i->ID ); ?>"
+																	target="_blank"><span class="icon-linkedin"></span></a>
+																</li><?php } ?>
+															<?php if ( get_field( 'interlocuteur_fb', $i->ID ) ) { ?>
+																<li class="facebook"><a
+																	href="<?php the_field( 'interlocuteur_fb', $i->ID ); ?>"
+																	target="_blank"><span class="icon-facebook"></span></a>
+																</li><?php } ?>
+															<?php if ( get_field( 'interlocuteur_viadeo', $i->ID ) ) { ?>
+																<li class="viadeo"><a
+																	href="<?php the_field( 'interlocuteur_viadeo', $i->ID ); ?>"
+																	target="_blank"><span class="icon-viadeo"></span></a>
+																</li><?php } ?>
+														</ul>
+													</li>
+												</ul>
+											</div>
 										</div>
-										<div class="txt-titre-modal">Votre<br /> interlocuteur</div>
-									</div>
-									<div class="close-modal">
-										<div class="container-fond-close-modal">
-											<div class="fond-close-modal"></div>
+										<div class="titre-modal">
+											<div class="container-fond-titre-modal">
+												<div class="fond-titre-modal"></div>
+											</div>
+											<div class="txt-titre-modal">Votre<br/> interlocuteur</div>
 										</div>
-										<a href="#" id="lien-close-interlocuteur-modal"><span class="icon-close"></span></a>
-									</div>
-								</nav>
+										<div class="close-modal">
+											<div class="container-fond-close-modal">
+												<div class="fond-close-modal"></div>
+											</div>
+											<a href="#" id="lien-close-interlocuteur-modal"><span class="icon-close"></span></a>
+										</div>
+									</nav>
+								</div>
 							</div>
-						</div>
-					<?php } ?>
+							<?php
+						}
+					}
 
-				<?php } ?>
+				} ?>
 
 <?php get_footer(); ?>
