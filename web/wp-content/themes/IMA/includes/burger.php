8ff0ee3b88872f787776ffd4f1be0657699354dc
Clément Lemoine <clementlemoine.cl@gmail.com>
2020-02-26 18:08:13 +0100

    🐛 home link on mobile

--- web/wp-content/themes/IMA/includes/burger.php
+++ web/wp-content/themes/IMA/includes/burger.php
@@ -2,7 +2,9 @@
 	<div class='container'>
 		<a href="#" id="menu-responsive" class="svg">
 			<object id="svg-hamburger" type="image/svg+xml" data="<?php echo get_template_directory_uri(); ?>/svg/hamburger.svg" data-fallback="<?php echo get_template_directory_uri(); ?>/layoutImg/svg-fallback/hamburger.png"></object>
-			<object id="svg-logo-ima" type="image/svg+xml" data="<?php echo get_template_directory_uri(); ?>/svg/logo-ima.svg" data-fallback="<?php echo get_template_directory_uri(); ?>/layoutImg/svg-fallback/logo-ima.png"></object>
-		</a>
+        </a>
+        <a href='/' class="home-link svg">
+            <object id="svg-logo-ima" type="image/svg+xml" data="<?php echo get_template_directory_uri(); ?>/svg/logo-ima.svg" data-fallback="<?php echo get_template_directory_uri(); ?>/layoutImg/svg-fallback/logo-ima.png"></object>
+        </a>
 	</div>
 </div>
