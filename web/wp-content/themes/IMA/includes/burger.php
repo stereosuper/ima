3ed27041a8104a2f8799a5957a66a0f41cde8665
ElisabethStereosuper <elisabeth@stereosuper.fr>
2020-05-12 15:08:31 +0200

    💄 logo responsive

--- web/wp-content/themes/IMA/includes/burger.php
+++ web/wp-content/themes/IMA/includes/burger.php
@@ -4,7 +4,7 @@
 			<object id="svg-hamburger" type="image/svg+xml" data="<?php echo get_template_directory_uri(); ?>/svg/hamburger.svg" data-fallback="<?php echo get_template_directory_uri(); ?>/layoutImg/svg-fallback/hamburger.png"></object>
         </a>
         <a href='/' class="home-link svg">
-            <object id="svg-logo-ima" type="image/svg+xml" data="<?php echo get_template_directory_uri(); ?>/svg/logo-ima.svg" data-fallback="<?php echo get_template_directory_uri(); ?>/layoutImg/svg-fallback/logo-ima.png"></object>
+            <img src='<?php echo get_template_directory_uri(); ?>/layoutImg/imatech-baseline.png"' alt='' />
         </a>
         <button id='link-search' class='link-search c-menu__search-button'></button>
 	</div>
